80df163db3dff9dc7550daba9fcef874f34dc077
Brian Smith <432a234@gmail.com>
2016-01-17 02:22:34 -0800

    Simplified code

--- src/validation/Validator.php
+++ src/validation/Validator.php
@@ -10,37 +10,32 @@ class Validator {
 
       foreach ($validation_data as $name => $value) {
 
-        $rules = explode("|", $value);
+        $exploded = explode(":", $value);
 
-        foreach ($rules as $rule) {
-          $exploded = explode(":", $rule);
+        switch ($exploded[0]) {
+          case 'min':
+              $min = $exploded[1];
+              if (Valid::alpha()->length($min)->Validate($_REQUEST[$name]) == false) {
+                  $errors[] = $name . " must be at least " . $min . " characters long!";
+              }
+              break;
 
-          switch ($exploded[0]) {
-            case 'min':
-                $min = $exploded[1];
-                if (Valid::alpha()->length($min)->Validate($_REQUEST[$name]) == false) {
-                    $errors[] = $name . " must be at least " . $min . " characters long!";
-                }
-                break;
+          case 'email':
+              if (Valid::email()->Validate($_REQUEST[$name]) == false) {
+                $errors[] = $name . " must be a valid email!";
+              }
+              break;
 
-            case 'email':
-                if (Valid::email()->Validate($_REQUEST[$name]) == false) {
-                  $errors[] = $name . " must be a valid email!";
-                }
-                break;
+          case 'equalTo':
+              if (Valid::equals($_REQUEST[$name])->Validate($_REQUEST[$exploded[1]]) == false) {
+                $errors[] = "Value does not match verification value!";
+              }
+              break;
 
-            case 'equalTo':
-                if (Valid::equals($_REQUEST[$name])->Validate($_REQUEST[$exploded[1]]) == false) {
-                  $errors[] = "Value does not match verification value!";
-                }
-                break;
-
-            default:
-                // do nothing
-          }
+          default:
+              $errors[] = "No value found!";
         }
       }
-
-    return $errors;
+      return $errors;
   }
 }
